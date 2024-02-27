62e526d5a671166df3cae0e70fcaa3571ce6626a
sarbada1 <sanjelsarbada12@gmail.com>
2024-02-27 10:48:49 +0545

    update

---
+++ condition/Largest.java
@@ -0,0 +1,16 @@
+package condition;
+
+public class Largest {
+    public static void main(String[] args) {
+        int a=10, b=20, c=5;
+        if(a>b && a>c){
+            System.out.printf("%d is largest", a);
+        }
+        else if(b>a && b>c){
+            System.out.printf("%d is largest", b);
+        }
+        else{
+            System.out.printf(" %d is  largest", c);
+        }
+    }
+}
